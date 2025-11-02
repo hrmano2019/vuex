da34992128e6c60c4b752eb92dc15547e98484b9
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2025-11-02 22:30:17 +0300

    Create authentic.js

---
+++ authentic.js
@@ -0,0 +1,10 @@
+import AuthLayout from '@/components/AuthLayout';
+
+export default function AuthPage() {
+  return (
+    <AuthLayout>
+      {/* Your auth form or content goes here */}
+      <div className="text-white p-10">Login or Register</div>
+    </AuthLayout>
+  );
+}
