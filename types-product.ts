dadb4b7a4d5b1d752e502b0fdb45df3e78162dd7
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2025-11-04 10:43:49 +0300

    Create types-product.ts

---
+++ types-product.ts
@@ -0,0 +1,14 @@
+// types/product.ts
+export interface ProductWithImages {
+  id: string;
+  description: string;
+  images: string;
+  price: number;
+  brand: string;
+  slug: string;
+  stock: number;
+  tags: string;
+  name: string;
+  type: string;
+  user: string;
+}
