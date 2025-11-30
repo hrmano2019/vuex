200a28b68b56fd13e85b01205dea1dcf8631ef58
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2025-11-30 14:27:23 +0300

    Update config-firebase.ts

--- .github/config-firebase.ts
+++ .github/config-firebase.ts
@@ -1,18 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXX",
-  authDomain: "inventory-app.firebaseapp.com",
-  databaseURL: "https://inventory-app-default-rtdb.firebaseio.com",
-  projectId: "inventory-app",
-  storageBucket: "inventory-app.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export Realtime Database
-export const db = getDatabase(app);
