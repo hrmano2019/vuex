2ce6790d934f64b7bfe286a72462ec01bd007b28
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2026-02-26 13:07:01 +0300

    Add EmploiForm component for job application

---
+++ types/test/EmploiForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="apply">
+    <input v-model="name" placeholder="Your Name" required />
+    <input v-model="email" placeholder="Your Email" required />
+    <textarea v-model="motivation" placeholder="Motivation Letter"></textarea>
+    <button type="submit">Apply</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return { name: "", email: "", motivation: "" };
+  },
+  methods: {
+    async apply() {
+      const res = await fetch("/api/apply", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.$data),
+      });
+      if (res.ok) alert("Application submitted!");
+    },
+  },
+};
+</script>
