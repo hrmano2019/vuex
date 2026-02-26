f8065b7f7c75a7b0bb6172dc97cd3275d8d98075
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2026-02-26 12:18:29 +0300

    Add document.vue for displaying documents

---
+++ docs/document.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Documents</h1>
+    <DocumentList :docs="docs" />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import DocumentList from "../components/DocumentList.vue";
+
+export default {
+  components: { DocumentList },
+  computed: {
+    ...mapState("documents", ["docs"]),
+  },
+  created() {
+    this.loadDocs();
+  },
+  methods: {
+    ...mapActions("documents", ["loadDocs"]),
+  },
+};
+</script>
