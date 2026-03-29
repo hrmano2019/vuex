a7d52c463f0add55e07650cdf9a94f9029c5d938
BADETE HERMANO <techsystemappliedecommergyherm@gmail.com>
2026-03-29 13:49:24 +0300

    Add index-page.tsx with main layout components

---
+++ src/index-page.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ProblemSection from "@/components/ProblemSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import ImpactSection from "@/components/ImpactSection";
+import GovernmentSection from "@/components/GovernmentSection";
+import TeamSection from "@/components/TeamSection";
+import CTASection from "@/components/CTASection";
+import Footer from "@/components/Footer";
+
+const Index = () => (
+  <main>
+    <Navbar />
+    <HeroSection />
+    <ProblemSection />
+    <HowItWorksSection />
+    <GovernmentSection />
+    <ImpactSection />
+    <TeamSection />
+    <CTASection />
+    <Footer />
+  </main>
+);
+
+export default Index;
